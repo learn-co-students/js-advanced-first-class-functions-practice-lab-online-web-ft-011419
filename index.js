6374b62faeee43ac533bad213fcda7470d2ac096
Sim Greenbaum <gbs4ever2@gmail.com>
2019-04-30 04:59:02 +0000

    Done.

--- index.js
+++ index.js
@@ -4,9 +4,29 @@ function logDriverNames(driver) {
   console.log(el.name)}
 }
 function logDriversByHometown(drivers, location){
-  drivers.foreach(fucntion (driver){
-    if(driver.hometown === location){
+  drivers.forEach(function (driver){
+    if (driver.hometown === location){
     console.log(driver.name)
     }
   });
+}
+function driversByRevenue(drivers){
+return  drivers.slice().sort(function(a,b) {
+  return a.revenue - b.revenue
+
+  })
+}
+function driversByName(drivers){
+return  drivers.slice().sort(function(a,b) {
+   return a.name.localeCompare(b.name)
+
+  })
+}
+const totalRevenue = function(drivers){
+return drivers.reduce(function(total,el){
+  return total + el.revenue
+},0)
+}
+const averageRevenue = function(drivers){
+return   totalRevenue(drivers) /drivers.length
 }
