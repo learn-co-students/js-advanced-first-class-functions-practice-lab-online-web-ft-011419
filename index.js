f39211205b0d58d96ce8de74e2a0bf15ec299193
Sim Greenbaum <gbs4ever2@gmail.com>
2019-04-30 03:30:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+function logDriverNames(driver) {
+  for (const el of driver){
+  console.log(el.name)}
+}
+function logDriversByHometown(drivers, location){
+  drivers.foreach(fucntion (driver){
+    if(driver.hometown === location){
+    console.log(driver.name)
+    }
+  });
+}
